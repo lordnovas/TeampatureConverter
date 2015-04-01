fc2d4df091be275170fc94758a4c94963ff79083
joel rainey <joelrainey@gmail.com>
2015-04-01 12:29:48 -0700

    removed unused imports

--- app/src/main/java/jr/com/teampatureconverter/LoginScreen.java
+++ app/src/main/java/jr/com/teampatureconverter/LoginScreen.java
@@ -15,34 +15,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.text.InputType;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Map;
-import java.util.Scanner;
 
 
 public class LoginScreen extends ActionBarActivity
 {
     EditText et_userID, et_password;
     Button mBtn_login;
-    private static final String TAG = "LoginScreen";
     int counter = 0;
     private ArrayList<String> ls_names = new ArrayList<>();
 
@@ -108,7 +97,6 @@ public class LoginScreen extends ActionBarActivity
         {
             ls_names.add(entry.getValue().toString().trim());
         }
-        ls_names.add("joel");
         return ls_names.contains(name);
     }
 
@@ -171,9 +159,7 @@ public class LoginScreen extends ActionBarActivity
 
         for(int i=0;i<ls_names.size()-1;i++)
         {
-            System.out.println(ls_names.get(i));
             editor.putString(""+counter++, ls_names.get(i));
-
             editor.commit();
         }
     }
